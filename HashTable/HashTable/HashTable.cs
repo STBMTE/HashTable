9c7b1114410af494403f067500fa72677b831b94
STBMTE <ateruhin388@gmail.com>
2021-12-21 10:20:59 +0300

    PUSH

---
+++ HashTable/HashTable/HashTable.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    class HashTable <TKey, TValue>
+    {
+        private TValue[] Items = new TValue[2000];
+
+        public TValue[] GetItems()
+        {
+            return Items;
+        }
+
+        public void Add(TKey key, TValue item)
+        {
+            int HashCode = HashFucntion(key);
+            int index = HashCode % 999;
+            while (index < Items.Length)
+            {
+                if (!Items[index].Equals(0))
+                {
+                    index++;
+                }
+                else
+                {
+                    Items[index] = item;
+                    break;
+                }
+            }
+        }
+
+        public bool Search(TKey key, TValue value)
+        {
+            int hashKey = HashFucntion(key);
+            int breakCounter = 0;
+            for(int i = hashKey; i < Items.Length; i++)
+            {
+                if (Items[i].Equals(value))
+                {
+                    return true;
+                }
+                if (Items[i].Equals(0))
+                {
+                    breakCounter++;
+                }
+                if(breakCounter >= 3)
+                {
+                    break;
+                }
+            }
+            return false;
+        }
+
+        public void Deleat(TKey key, TValue value)
+        {
+            int hashKey = HashFucntion(key);
+            for(int i = hashKey; i < Items.Length; i++)
+            {
+                if (Items[i].Equals(value))
+                {
+                    Items[i] = "";
+                }
+            }
+        }
+
+        public int HashFucntion(TKey key)
+        {
+            var Random = new Random();
+            string Item = Convert.ToString(key);
+            int result = 0;
+            for(int i = 0; i < Item.Length; i++)
+            {
+                result = ((((Item[i] << 55) - i) >> 12) * i) % Items.Length - 1; 
+            }
+            return Math.Abs(result);
+        }
+    }
+}
---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HashTable
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var hash = new HashTable<int, int>();
+            for(int i = 0; i < 1000; i++)
+            {
+                hash.Add(i, 51216 - i);
+            }
+            var Items = hash.GetItems();
+            foreach (var item in Items)
+            {
+                if (item != 0)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
